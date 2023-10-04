96344e1464d9c5b54fd0e89f5c2aa045bb56e7e5
Shayan <shayanakhlaghian@gmail.com>
2023-10-04 13:55:57 +0330

    change background to night and font to lato

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Lato } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] });
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['100', '300', '400', '700'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={`${inter.className} bg-gray-900 text-white`}>
+      <body className={`${lato.className} bg-night text-white`}>
         {children}
       </body>
     </html>
