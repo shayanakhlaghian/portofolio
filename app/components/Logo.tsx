c8e283ea265869dfe5ca8e61c29cff7c68f3e0e1
Shayan <shayanakhlaghian@gmail.com>
2023-10-05 01:10:52 +0330

    create logo component

---
+++ app/components/Logo.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+
+const Logo = () => {
+  return (
+    <div className='w-32 h-32 md:w-64 md:h-64 lg:w-96 lg:h-96 absolute bottom-[-4rem] left-1/2 translate-x-[-50%] md:bottom-[-.5rem] md:left-[8%] animate-rotate'>
+      <Image src='/logo.svg' alt='Logo' fill />
+    </div>
+  );
+};
+export default Logo;
