5f6264b2162ba8f3d87f091cb820ce1362714b43
Shayan <shayanakhlaghian@gmail.com>
2023-10-05 00:21:28 +0330

    add nav to menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,13 +1,20 @@
 'use client';
+
 import { useState } from 'react';
 
 import Toggle from './Toggle';
+import Nav from './Nav';
 
 const Menu = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggle = () => setIsOpen((isOpen) => !isOpen);
 
-  return <Toggle isOpen={isOpen} onClick={handleToggle} />;
+  return (
+    <>
+      <Toggle isOpen={isOpen} onClick={handleToggle} />
+      <Nav isOpen={isOpen} />
+    </>
+  );
 };
 export default Menu;
