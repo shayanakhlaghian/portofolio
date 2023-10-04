e8cc568225b9f0c14343def74b7cd80b18d5875a
Shayan <shayanakhlaghian@gmail.com>
2023-10-04 16:52:21 +0330

    add headings and about component to hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,13 +1,51 @@
+'use client';
+import type { Variants, Transition } from 'framer-motion';
+import { motion } from 'framer-motion';
+
+import About from './About';
+
+const variants: Variants = {
+  hide: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 1,
+    },
+  },
+};
+
+const transition: Transition = { duration: 1 };
+
 const Hero = () => {
   return (
-    <section className='py-2 md:px-4'>
-      <h1 className='text-2xl md:text-4xl lg:text-7xl tracking-wide mb-1 md:mb-2 font-light'>
+    <motion.section
+      className='py-2 md:px-4 h-full relative'
+      variants={variants}
+      initial='hide'
+      animate='show'
+    >
+      <motion.h1
+        className='text-2xl md:text-4xl lg:text-7xl tracking-wide mb-1 md:mb-2 font-light'
+        variants={variants}
+        transition={transition}
+      >
         Shayan Akhlaghian
-      </h1>
-      <h2 className='text-sm md:text-2xl font-bold'>
+      </motion.h1>
+      <motion.h2
+        className='text-sm md:text-2xl font-bold [&>*]:mr-2 last:mr-0'
+        variants={variants}
+        transition={transition}
+      >
         Web Developer | Designer
-      </h2>
-    </section>
+      </motion.h2>
+      <motion.div
+        className='absolute w-full top-[40%] md:w-1/2 md:left-1/2 lg:w-1/3 lg:left-[60%]'
+        variants={variants}
+        transition={transition}
+      >
+        <About />
+      </motion.div>
+    </motion.section>
   );
 };
 export default Hero;
