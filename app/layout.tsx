38973d7247d7e517440ec3c4df8eded03943ba74
Shayan <shayanakhlaghian@gmail.com>
2023-10-04 14:13:21 +0330

    add rounded edges to frame

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
       <body
         className={`${lato.className} bg-night text-white h-[90vh] md:h-[92.5vh]`}
       >
-        <div className='border-2 border-white m-6 h-full p-4'>{children}</div>
+        <div className='border-2 border-white m-6 h-full p-4 rounded-sm'>
+          {children}
+        </div>
       </body>
     </html>
   );
