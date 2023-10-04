441ef10bb5258f63f9db7a794cbfc40139cc3ad8
Shayan <shayanakhlaghian@gmail.com>
2023-10-04 14:47:39 +0330

    add headings to hero

---
+++ app/components/Hero.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+  return (
+    <section className='py-2 md:px-4'>
+      <h1 className='text-2xl md:text-4xl lg:text-7xl tracking-wide mb-1 md:mb-2 font-light'>
+        Shayan Akhlaghian
+      </h1>
+      <h2 className='text-sm md:text-2xl font-bold'>
+        Web Developer | Designer
+      </h2>
+    </section>
+  );
+};
+export default Hero;
