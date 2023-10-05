5cfbe18eb1ff648fa53582efaaee34c7bd67b573
Shayan <shayanakhlaghian@gmail.com>
2023-10-05 11:21:07 +0330

    change nav item styling

--- app/components/NavItem.tsx
+++ app/components/NavItem.tsx
@@ -1,13 +1,4 @@
-import type { Transition, Variants } from 'framer-motion';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
-
-const variants: Variants = {
-  close: { opacity: 0 },
-  open: { opacity: 1 },
-};
-
-const transition: Transition = { duration: 2 };
 
 const NavItem = ({
   children,
@@ -17,19 +8,14 @@ const NavItem = ({
   href: string;
 }) => {
   return (
-    <motion.li
-      className='mb-6 md:mb-8 lg:mb-10 relative group cursor-pointer'
-      variants={variants}
-      transition={transition}
-    >
-      <span className='w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 bg-white rounded-full inline-block absolute translate-x-[-3rem] md:translate-x-[-3.5rem] lg:translate-x-[-4rem] top-1/2 translate-y-[-50%] group-hover:w-[150%] duration-200 z-10' />
+    <li className='mb-6 md:mb-8 lg:mb-10 relative cursor-pointer border-2 text-center py-2 px-4 rounded-full hover:bg-white duration-200 group'>
       <Link
         href={href}
-        className='font-poiret text-2xl md:text-3xl lg:text-4xl font-bold group-hover:text-wisteria z-20 relative duration-200'
+        className='font-poiret text-2xl md:text-3xl lg:text-4xl font-bold z-20 relative group-hover:bg-gradient-to-r group-hover:from-wisteria-light group-hover:to-wisteria-dark group-hover:bg-clip-text group-hover:text-transparent duration-200'
       >
         {children}
       </Link>
-    </motion.li>
+    </li>
   );
 };
 export default NavItem;
