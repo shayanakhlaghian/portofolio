eb7f88e5496e74d02093cb3bbb553ae33d5565bb
Shayan <shayanakhlaghian@gmail.com>
2023-10-05 14:06:28 +0330

    change primary button color

--- app/components/PrimaryButton.tsx
+++ app/components/PrimaryButton.tsx
@@ -7,7 +7,7 @@ const PrimaryButton = ({
 }) => {
   return (
     <button
-      className='text-secondary-dark font-bold relative text-sm lg:text-lg group'
+      className='text-tertiary-dark font-bold relative text-sm lg:text-lg group'
       onClick={onClick}
     >
       <span className='w-6 h-6 lg:w-7 lg:h-7 bg-white rounded-full absolute z-[2] top-1/2 translate-y-[-50%] translate-x-[-10%] group-hover:w-[120%] duration-200' />
